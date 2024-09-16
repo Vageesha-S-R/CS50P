0edb031f348fda759412a32405e8eac5342c95bf
Vageesha S R <176924830+Vageesha-S-R@users.noreply.github.com>
2024-09-16 18:13:28 +0530

    Create test_numb3rs.py

---
+++ week 7/numb3rs/test_numb3rs.py
@@ -0,0 +1,17 @@
+from numb3rs import validate
+
+def test_format():
+    assert validate(r"1.2.3.4")==True
+    assert validate(r"1.2.3")==False
+    assert validate(r"1.2")==False
+    assert validate(r"1")==False
+    assert validate(r"cat")==False
+
+def test_range():
+    assert validate(r"255.255.255.255")==True
+    assert validate(r"512.1.1.1")==False
+    assert validate(r"1.512.1.1")==False
+    assert validate(r"1.1.512.1")==False
+    assert validate(r"1.1.1.512")==False
+
+
